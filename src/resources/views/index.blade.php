f4b9d9b0e8122d243a2940454cc5a11b3313331d
daiki203 <daikikarasuyama@gmail.com>
2025-03-22 16:36:32 +0900

    first commit

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>名古屋大学軟式野球部紹介</title>
+</head>
+<body>
+    
+</body>
+</html>
